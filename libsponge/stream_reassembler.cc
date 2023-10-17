a03c2077df77c42081355d0bb561a7747cdf054e
ZhangIvan1 <zhang_ivan1@163.com>
2023-10-17 06:31:05 +0000

    pass lab1 check

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,41 +12,40 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : buffer_string(""), buffer_check(""), _reassembled_pt(0), _end_pt(0), _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : buffer_string(""), buffer_check(""), _reassembled_pt(0), _end_pt(-1), _output(capacity), _capacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
 //    DUMMY_CODE(data, index, eof);
+    size_t expended_length = index + data.length();
+    size_t max_expended_length = _reassembled_pt + _capacity;
 
-    size_t extended_length = index + data.length();
-
-    if (index > _reassembled_pt + _output.remaining_capacity())
+    if (index > max_expended_length)
         return;
 
-    extended_length = min(extended_length, _reassembled_pt + _output.remaining_capacity());
+    if (eof && expended_length <= max_expended_length)
+        _end_pt = expended_length;
 
-    if (eof)
-        _end_pt = extended_length;
-
-
-    if (extended_length > _capacity){
-        buffer_string.resize(extended_length);
-        buffer_check.resize(extended_length);
+    if (expended_length > buffer_string.length() ) {
+        buffer_string.resize(expended_length);
+        buffer_check.resize(expended_length);
     }
 
-    buffer_string.replace(index,data.length(),data);
-    buffer_check.replace(index,data.length(),data.length(),'1');
+        buffer_string.replace(index, data.length(), data);
+        buffer_check.replace(index, data.length(), data.length(), '1');
 
-    if (extended_length < _capacity){
-        buffer_string.resize(extended_length);
-        buffer_check.resize(extended_length);
+    if (buffer_string.length() > max_expended_length){
+        buffer_string.resize(max_expended_length);
+        buffer_check.resize(max_expended_length);
     }
 
-    while(buffer_check[_reassembled_pt] && _output.remaining_capacity()){
-        _output.write(to_string(buffer_string[_reassembled_pt]));
-        _reassembled_pt++;
+    if (buffer_check[_reassembled_pt] && _output.remaining_capacity()){
+        size_t write_len = 0;
+        while(buffer_check[_reassembled_pt + write_len] && _output.remaining_capacity() - write_len) write_len++;
+        _output.write(buffer_string.substr(_reassembled_pt,write_len));
+        _reassembled_pt += write_len;
     }
 
     if (_reassembled_pt == _end_pt)
@@ -54,9 +53,9 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
 }
 
 size_t StreamReassembler::unassembled_bytes() const {
-    size_t bytes_cnt=0;
-    for (size_t i = _reassembled_pt; i <=_end_pt; i++)
-        if(buffer_check[i]=='1')
+    size_t bytes_cnt = 0;
+    for (size_t i = _reassembled_pt; i != buffer_check.length(); i++)
+        if(buffer_check[i] == '1')
             bytes_cnt++;
     return bytes_cnt;
 }
