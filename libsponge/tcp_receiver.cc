183f84948b684449ed7041ce7ce5245290dc512b
ZhangIvan1 <zhang_ivan1@163.com>
2023-10-23 10:46:52 +0000

    Lab2 TCPReceiver fix

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -15,16 +15,16 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     if (seg.header().syn && !_syn) {
         _isn = seg.header().seqno;
         _syn = seg.header().syn;
-    } else if (!_syn || _fin) return;
+    } else if (!_syn) return;
     _reassembler.push_substring(seg.payload().copy(),
                                 unwrap(seg.header().seqno, _isn, _reassembler.stream_out().bytes_written() + 1) - 1 +
                                     seg.header().syn,
                                 seg.header().fin);
-    if (seg.header().fin && _reassembler.empty()) _fin = seg.header().fin;
+    _fin = seg.header().fin;
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
-    return _syn ? optional<WrappingInt32>{wrap(_reassembler.stream_out().bytes_written() + _syn + _fin, _isn)}
+    return _syn ? optional<WrappingInt32>{wrap(_reassembler.stream_out().bytes_written() + _syn + (_fin && _reassembler.empty()), _isn)}
                 : nullopt;
 }
 
